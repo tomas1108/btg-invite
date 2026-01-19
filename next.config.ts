f34d3faf5f4e2dc2bf44a050acba5533e5070b58
Ninh Hoang Khai <nhkkhaii@gmail.com>
2026-01-19 22:28:30 +0700

    cache Component PPR

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  cacheComponents: false,
+  cacheComponents: true,
   reactCompiler: true,
 
 };
